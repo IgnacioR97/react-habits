9caf55386b13ea658f3d8173cffcaf8a42f5289d
IgnacioR97 <ignaciorevello21@gmail.com>
2023-03-28 08:50:57 -0700

    15 minutes done

--- src/cards/Tips.jsx
+++ src/cards/Tips.jsx
@@ -1,7 +1,14 @@
 const Tips = () => {
   return (
     <div className="card card-tips">
-      <h2>new tips</h2>
+      <h2 className="tips-header">Become More Productive</h2>
+      <p className="tips-info">
+        Here are some simple tips to highly increase your productivity with
+        ease.
+      </p>
+      <div className="btn-container">
+        <button className="tips-btn">view tips</button>
+      </div>
     </div>
   );
 };
