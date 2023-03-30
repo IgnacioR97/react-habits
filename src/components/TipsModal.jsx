189044e79922d4b50f275009185012dda91765c4
IgnacioR97 <ignaciorevello21@gmail.com>
2023-03-30 07:32:59 -0700

    15 minutes started

--- src/components/TipsModal.jsx
+++ src/components/TipsModal.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import tips from "../tips";
+import { RxArrowLeft, RxArrowRight } from "react-icons/rx";
+import { MdOutlineClose } from "react-icons/md";
+
 const TipsModal = ({ setOpenModal }) => {
+  const [currentNum, setCurrentNum] = useState(0);
+  const { tip } = tips[currentNum];
+
   return (
     <>
       <div className="background" onClick={() => setOpenModal(false)}></div>
-      <div className="modal">{/* <h2>tips</h2> */}</div>
+      <div className="modal">
+        <h4>{tip}</h4>
+        <div className="buttons">
+          <button
+            className="btn-left"
+            onClick={() => setCurrentNum(1 - currentNum)}
+          >
+            <RxArrowLeft />
+          </button>
+          <button
+            className="btn-right"
+            onClick={() => setCurrentNum(1 + currentNum)}
+          >
+            <RxArrowRight />
+          </button>
+        </div>
+        <button className="close-btn" onClick={() => setOpenModal(false)}>
+          <MdOutlineClose />
+        </button>
+      </div>
     </>
   );
 };
---
+++ src/tips.js
@@ -0,0 +1,28 @@
+const tips = [
+  {
+    id: 0,
+    tip: "Here are some productivity tips to improve your focus and concentration",
+  },
+  {
+    id: 1,
+    tip: "Leave your phone in another room when you study so you don't get distracted. Don't worry, your friends can wait for 45 minutes.",
+  },
+  {
+    id: 2,
+    tip: "Take a break every 45 minutes as it is your maximum attention span. Make sure to go outside and don't stay looking at your screen as you brain doesn't actually rest.",
+  },
+  {
+    id: 3,
+    tip: `Turn do not disturb on your laptop. Studying and completing this task is your #1 priority, so letting people interfere for just 45 minutes is slowly "killing" your productivity`,
+  },
+  {
+    id: 4,
+    tip: "Listen to music that has no lyrics, or just listen to nothing. Sometimes, silence is golden.",
+  },
+  {
+    id: 5,
+    tip: "Don't multitask. If you're doing work, do work. If you're watching a movie, watch a movie. If you're doing work and watching a movie, you're doing neither.",
+  },
+];
+
+export default tips;
